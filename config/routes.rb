2ecbeaa547aad5ed1c2bbb1faf5627e1eb380b57
michellelle <upgrade2mode@gmail.com>
2016-07-12 01:41:43 +0000

    add products location, rout, folders, layouts, and improved about page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   get '/about' => 'pages#about'
-  root 'pages#home'
+  root 'products#index'
+  
+  resources :products, only: [:show] 
+  
+  post 'quote', to: 'products#quote' 
+  post 'order', to: 'products#order' 
+  get 'done', to: 'products#done' 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
